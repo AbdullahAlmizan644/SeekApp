27fd43c6a8c58613ae2594fe3eabb2041a51a977
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2022-12-22 17:11:06 +0600

    first

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from 'react-native-webview';
+ 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <View style={{ width:'100%', height:'100%'}}>
+       <WebView
+        source={{ uri: 'http://seek.pythonanywhere.com/' }}
+      />
+
+       </View>
+
       <StatusBar style="auto" />
+      
     </View>
   );
 }
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +33,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
